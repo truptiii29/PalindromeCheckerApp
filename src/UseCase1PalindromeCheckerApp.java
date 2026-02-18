0448fc4b4be408ebcedca7c18b1b440edaa76558
truptiii29 <jtrupti029@gmail.com>
2026-02-18 15:31:02 +0530

    UC1: Add welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hey there! Welcome to my Palindrome Checker :)");
+        System.out.println("This little app checks if a word or phrase reads the same backwards.");
+        System.out.println("Try fun words like 'madam' or 'racecar'!");
+        System.out.println("Yay! The application started successfully!");
+        System.out.println("Let's have some fun with palindromes");
+    }
+}
+
