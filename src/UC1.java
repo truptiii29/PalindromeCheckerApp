9e186ed71b2d032a6a048a3d8f58f586e771f5c2
truptiii29 <jtrupti029@gmail.com>
2026-02-18 16:15:14 +0530

    UC7

---
+++ src/UC1.java
@@ -0,0 +1,11 @@
+public class UC1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hey there! Welcome to my Palindrome Checker :)");
+        System.out.println("This little app checks if a word or phrase reads the same backwards.");
+        System.out.println("Try fun words like 'madam' or 'racecar'!");
+        System.out.println("Yay! The application started successfully!");
+        System.out.println("Let's have some fun with palindromes");
+    }
+}
